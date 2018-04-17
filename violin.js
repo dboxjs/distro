7c433add64c386a998dd0004ca7c8cc13b19a323
Alecs Garza <alecsgarza@Alecss-iMac.local>
2018-04-17 08:56:49 -0500

    Add all possible chart versions of distrochart

--- violin.js
+++ violin.js
@@ -107,24 +107,68 @@ export default function(config, helper) {
     //vm.chart.svg().call(vm._tip);
     
     var chart1 = makeDistroChart({
-        helper: vm.helper,
         data:vm._data,
         xName:'x',
         yName:'y',
-        //axisLabels: {xAxis: null, yAxis: null},
-        //selector:"#chart-distro1",
-        //chartSize:{height:460, width:960},
+        axisLabels: {xAxis: null, yAxis: null},
+        selector:"#chart-distro1",
+        chartSize:{height:460, width:960},
         constrainExtremes:true});
+
     chart1.renderBoxPlot();
     chart1.renderDataPlots();
-    /* 
     chart1.renderNotchBoxes({showNotchBox:false});
     chart1.renderViolinPlot({showViolinPlot:false});
+    
+    //Box Plot
+    /* chart1.violinPlots.hide();
+    chart1.boxPlots.show({reset:true});
+    chart1.notchBoxes.hide();
+    chart1.dataPlots.change({showPlot:false,showBeanLines:false});  */
 
-    chart1.violinPlots.show({reset:true,clamp:0});
+    //Notched Box Plot
+   /*  chart1.violinPlots.hide();
+    chart1.notchBoxes.show({reset:true});
+    chart1.boxPlots.show({reset:true, showBox:false,showOutliers:true,boxWidth:20,scatterOutliers:true});
+    chart1.dataPlots.change({showPlot:false,showBeanLines:false}); */
+
+    //Violin Plot Unbound
+    /* chart1.violinPlots.show({reset:true,clamp:0});
     chart1.boxPlots.show({reset:true, showWhiskers:false,showOutliers:false,boxWidth:10,lineWidth:15,colors:['#555']});
     chart1.notchBoxes.hide();
-    chart1.dataPlots.change({showPlot:false,showBeanLines:false}) */
+    chart1.dataPlots.change({showPlot:false,showBeanLines:false})  */
+
+    //Violin Plot Clamp to Data
+    /* chart1.violinPlots.show({reset:true,clamp:1});
+    chart1.boxPlots.show({reset:true, showWhiskers:false,showOutliers:false,boxWidth:10,lineWidth:15,colors:['#555']});
+    chart1.notchBoxes.hide();
+    chart1.dataPlots.change({showPlot:false,showBeanLines:false}); */
+
+    //Bean Plot
+    /* chart1.violinPlots.show({reset:true, width:75, clamp:0, resolution:30, bandwidth:50});
+    chart1.dataPlots.show({showBeanLines:true,beanWidth:15,showPlot:false,colors:['#555']});
+    chart1.boxPlots.hide();
+    chart1.notchBoxes.hide(); */
+
+    //Beeswarm Plot
+    /* chart1.violinPlots.hide();
+    chart1.dataPlots.show({showPlot:true, plotType:'beeswarm',showBeanLines:false, colors:null});
+    chart1.notchBoxes.hide();
+    chart1.boxPlots.hide(); */
+    
+    //Scatter Plot
+    chart1.violinPlots.hide();
+    chart1.dataPlots.show({showPlot:true, plotType:40, showBeanLines:false,colors:null});
+    chart1.notchBoxes.hide();
+    chart1.boxPlots.hide();
+    
+    //Trend Lines
+    /* if(chart1.dataPlots.options.showLines){
+        chart1.dataPlots.change({showLines:false});
+    } else {
+        chart1.dataPlots.change({showLines:['median','quartile1','quartile3']});
+    } */
+
 
     return vm;
   }
